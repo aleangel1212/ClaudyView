e69a60456a7379363ca4388d73f35c00afdebd9b
Alessandro Vecchi <aleangel1212@gmail.com>
2018-05-08 14:00:33 -0400

    Basic portfolio page

--- page-portfolio.php
+++ page-portfolio.php
@@ -1,10 +1,31 @@
 <?php get_header(); ?>
 
+<?php 
+	$args = array(
+		'numberposts' => -1,
+		'category_name' => 'portfolio'
+	);
+	
+	$posts = get_posts($args);
+?>
+
 <div class="cloud-header">
 	<img src=<?= get_stylesheet_directory_uri()."/public/images/clouds-header.png" ?>>
 </div>
-<div class="content portfolio-content">
-	
-</div>
+<section class="section portfolio-content has-text-centered">
+	<?php
+		foreach ($posts as $post) {
+	?>
+
+		<a href=<?= get_permalink($post) ?>>
+			<figure class="image portfolio-image">
+			  <img src=<?= get_the_post_thumbnail_url($post) ?>>
+			</figure>
+		</a>
+		
+	<?php
+		}
+	?>
+</section>
 
 <?php get_footer(); ?>
